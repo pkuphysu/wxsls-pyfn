929245da0c4de308d8ba05db9cb205308af128c9
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2021-04-22 22:29:55 +0800

    fix: restore naive db create api for init db

--- src/pkuphysu_wechat/tasks/views.py
+++ src/pkuphysu_wechat/tasks/views.py
@@ -5,6 +5,7 @@ from typing import Callable
 from flask import Blueprint, abort, request
 
 from pkuphysu_wechat.config import settings
+from pkuphysu_wechat import db
 from pkuphysu_wechat.wechat import wechat_client
 
 bp = Blueprint("tasks", __name__, url_prefix="/tasks")
@@ -29,3 +30,11 @@ def menu():
         menu_data = f.read()
     wechat_client.create_menu(menu_data)
     return "Menu Updated"
+
+
+@bp.route("/db/create")
+@auth_required
+def db_create():
+    db.create_all()
+    return "DB Created"
+
